50a00dda1cf93d9762c6fd9b91b0f110ea9067ab
Yuikito <72788470+Yuikito@users.noreply.github.com>
2020-10-13 17:22:58 +0700

    Add files via upload

---
+++ Bai_1.py
@@ -0,0 +1,8 @@
+print("Mời bạn nhập số a:");
+a=input();
+print("Mời bạn nhập số b:");
+b=input();
+x=int(a) + int(b);
+y=int(a) * int(b);
+print("kết quả của a + b là:",x);
+print("kết quả của a * b là:",y);
