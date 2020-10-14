23e2f3f7f1c680908cc1b8ad9ab52608605d8f0f
Yuikito <72788470+Yuikito@users.noreply.github.com>
2020-10-14 21:25:14 +0700

    Add files via upload

---
+++ Bài_1_2.0.py
@@ -0,0 +1,9 @@
+print("Mời bạn nhập số a:");
+a=input();
+print("Mời bạn nhập số b:");
+b=input();
+if (a!=float) and (b!=float):
+    print("Vui lòng nhập số")
+else:
+    print("kết quả của a + b là:",float(a) + float(b));
+    print("kết quả của a * b là:",float(a) * float(b));
