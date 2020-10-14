a9f0f6942a446014db1494ac6241a52ad9d14d8b
Yuikito <72788470+Yuikito@users.noreply.github.com>
2020-10-14 21:31:17 +0700

    Delete Bài_1_2.0.py

--- Bài_1_2.0.py
+++
@@ -1,9 +0,0 @@
-print("Mời bạn nhập số a:");
-a=input();
-print("Mời bạn nhập số b:");
-b=input();
-if (a!=float) and (b!=float):
-    print("Vui lòng nhập số")
-else:
-    print("kết quả của a + b là:",float(a) + float(b));
-    print("kết quả của a * b là:",float(a) * float(b));
